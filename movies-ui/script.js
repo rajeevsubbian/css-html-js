94dacd16a0b460b661597957c26a9b49c6c5b7fa
rajeevsubbian <rajeev.subbian@gmail.com>
2024-10-13 13:54:14 +0100

    movie app day 17 -search updated

--- movies-ui/script.js
+++ movies-ui/script.js
@@ -17,24 +17,24 @@ async function getMovies(url) {
 }
 
 function showMovies(movies) {
-    main.innerHMTL = ''
+    main.innerHTML = ''
 
     movies.forEach((movie) => {
         const { title, poster_path, vote_average, overview } = movie
 
         const movieEl = document.createElement('div')
         movieEl.classList.add('movie')
-        
+
         movieEl.innerHTML = `
             <img src="${IMG_PATH + poster_path}" alt="${title}">
             <div class="movie-info">
-                <h3>${title}</h3>
-                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
             </div>
             <div class="overview">
-                <h3>Overview</h3>
-                ${overview}
-            </div>
+          <h3>Overview</h3>
+          ${overview}
+        </div>
         `
         main.appendChild(movieEl)
     })
@@ -48,7 +48,7 @@ function getClassByRate(vote) {
     } else {
         return 'red'
     }
- }
+}
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -62,5 +62,4 @@ form.addEventListener('submit', (e) => {
     } else {
         window.location.reload()
     }
-
 })
