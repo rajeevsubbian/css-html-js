dd216e8005a6a822b93d38cc6393bc4e8627fa9e
rajeevsubbian <rajeev.subbian@gmail.com>
2024-11-01 13:46:38 +0000

    Hoverboard  Day 36

--- img-carousel/script.js
+++ img-carousel/script.js
@@ -6,7 +6,7 @@ const img = document.querySelectorAll('#imgs img')
 
 let idx = 0
 
-let interval = setInterval(run, 2000)
+let interval = setInterval(run, 5000)
 
 function run() {
     idx++
@@ -25,7 +25,7 @@ function changeImage() {
 
 function resetInterval() {
     clearInterval(interval)
-    interval = setInterval(run, 2000)
+    interval = setInterval(run, 5000)
 }
 
 rightBtn.addEventListener('click', () => {
