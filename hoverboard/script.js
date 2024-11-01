0de211085c0adc59caf038a803e177e878fac201
rajeevsubbian <rajeev.subbian@gmail.com>
2024-11-01 13:47:17 +0000

    Hoverboard + 1  Day 36

--- hoverboard/script.js
+++ hoverboard/script.js
@@ -1,6 +1,6 @@
 const container = document.getElementById('container')
-// const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
-const colors = ['blue', 'orange']
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
+// const colors = ['blue', 'orange']
 const SQUARES = 700
 
 for(let i = 0; i < SQUARES; i++) {
