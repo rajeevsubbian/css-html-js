8f660990e6c6dcad76faf027ecdce953e5fcb174
rajeevsubbian <rajeev.subbian@gmail.com>
2024-11-01 15:24:08 +0000

    Pokedex  Day 37

--- hoverboard/script.js
+++ hoverboard/script.js
@@ -1,6 +1,7 @@
 const container = document.getElementById('container')
-const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71', 'yellow', '#fff']
 // const colors = ['blue', 'orange']
+// const colors = ['white']
 const SQUARES = 700
 
 for(let i = 0; i < SQUARES; i++) {
